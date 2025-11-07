6ece34ad5d7340d002492c2610b4d4fccda999f2
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2025-11-07 18:27:46 +0530

    Origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173", // Frontend URL
+    origin: ["http://localhost:5173", "https://hrms.sosapient.in"], // Frontend URLs for development and production
     methods: ["GET", "POST"]
   }
 });
